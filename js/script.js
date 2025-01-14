8efc1a0c9489da2c4a3bada55e895f58fec8c59f
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2025-01-14 03:21:12 +0200

    8th commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 const menuBtn = document.getElementById('menu-btn')
 const mobileMenu = document.getElementById('mobile-menu')
 
-menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden')
-})
-document
-  .getElementById('contactForm')
-  .addEventListener('submit', function (event) {
+if (menuBtn) {
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden')
+  })
+}
+const contactForm = document.getElementById('contactForm')
+if (contactForm) {
+  contactForm.addEventListener('submit', function (event) {
     event.preventDefault()
 
     var name = document.getElementById('name').value
     var title = document.getElementById('title').value
     var message = document.getElementById('message').value
-    var phoneNumber = '0530349227'
+    var phoneNumber = '966530349227'
 
     var whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
       'الاسم: ' + name + '\nالموضوع: ' + title + '\nالرسالة: ' + message
     )}`
     window.open(whatsappURL, '_blank')
   })
+}
